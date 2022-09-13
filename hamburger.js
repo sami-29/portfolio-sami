ed33bc1e0f3e08b4b4a26e2662e42c9dea603500
sami-29 <annabisami01@gmail.com>
2022-09-13 14:34:48 +0100

    fixed hamburger button

--- hamburger.js
+++ hamburger.js
@@ -3,11 +3,9 @@ const menu = document.querySelector("#navbar-default");
 
 toggle_menu.onclick = () => {
   if (menu.classList.contains("hidden")) {
-    toggle = true;
     menu.classList.remove("hidden");
     console.log(menu);
   } else {
-    toggle = false;
     menu.classList.add("hidden");
     console.log(menu);
   }
