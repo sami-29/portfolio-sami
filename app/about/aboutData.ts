3ccc5dc8dd1abe37c72f0a44466bb538dd3d9a8c
sami-29 <annabisami01@gmail.com>
2022-11-20 19:24:28 +0100

    Major Changes and added new tools

---
+++ app/about/aboutData.ts
@@ -0,0 +1,117 @@
+import css from "../../public/assets/about/css.webp";
+import git from "../../public/assets/about/git.webp";
+import gsap from "../../public/assets/about/gsap.webp";
+import html from "../../public/assets/about/html.webp";
+import js from "../../public/assets/about/js.webp";
+import NextJs from "../../public/assets/about/NextJs.webp";
+import PocketBase from "../../public/assets/about/PocketBase.webp";
+import php from "../../public/assets/about/php.webp";
+import node from "../../public/assets/about/node.webp";
+import tailwind from "../../public/assets/about/tailwind.webp";
+import python from "../../public/assets/about/python.webp";
+import vite from "../../public/assets/about/vite.webp";
+import sass from "../../public/assets/about/sass.webp";
+import threejs from "../../public/assets/about/three.js.webp";
+import typescript from "../../public/assets/about/typescript.webp";
+import react from "../../public/assets/about/react.webp";
+import mongodb from "../../public/assets/about/mongodb.webp";
+import mysql from "../../public/assets/about/mysql.webp";
+
+const cards = [
+  {
+    title: "HTML5",
+    description: "Basic HTML (Hyper Text Markup Language)",
+    src: html,
+  },
+  {
+    title: "CSS3",
+    description: "Basic CSS (cascade style sheet)",
+    src: css,
+  },
+  {
+    title: "JavaScript",
+    description: "I have good knowledge of Javascript es6+",
+    src: js,
+  },
+  {
+    title: "React",
+    description: "I use React for single page and highly interactive web apps",
+    src: react,
+  },
+  {
+    title: "TypeScript",
+    description: "I use TypeScript for medium to larger scale web applications",
+    src: typescript,
+  },
+  {
+    title: "Git",
+    description:
+      "I use git to maintain and push my projects to my repositories",
+    src: git,
+  },
+  {
+    title: "SCSS",
+    description: "I use SCSS over CSS because of its extra features",
+    src: sass,
+  },
+  {
+    title: "Tailwind CSS",
+    description:
+      "I use Tailwind if i don't need very fancy css and wanna keep it minimalistic",
+    src: tailwind,
+  },
+  {
+    title: "Node.js",
+    description: "Node.js is my go to JavaScript runtime",
+    src: node,
+  },
+  {
+    title: "PHP",
+    description:
+      "I don't use php in my projects but i have enough knowledge about it",
+    src: php,
+  },
+  {
+    title: "Python",
+    description:
+      "I started programming with python and i am comfortable working with it",
+    src: python,
+  },
+  {
+    title: "Vite",
+    description: "I use vite as my bundler for high performance websites",
+    src: vite,
+  },
+  {
+    title: "Three.js",
+    description:
+      "Three.js is a JavaScript library used to create and display animated 3D computer graphics in the web",
+    src: threejs,
+  },
+  {
+    title: "Gsap",
+    description:
+      "I use Gsap for complex animations and better control over them",
+    src: gsap,
+  },
+  {
+    title: "Next Js",
+    description:
+      "I use Next js for static websites or as my fullstack framework",
+    src: NextJs,
+  },
+  {
+    title: "PocketBase",
+    description:
+      "Open Source backend in 1 file with realtime database, authentication, file storage and admin dashboard",
+    src: PocketBase,
+  },
+  {
+    title: "mongodb",
+    description:
+      "MongoDB is a source-available cross-platform document-oriented database program",
+    src: mongodb,
+  },
+];
+
+export default cards;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,112 +1,5 @@
 import AboutCard from "../../components/AboutCard";
-
-import css from "../../public/assets/about/css.webp";
-import git from "../../public/assets/about/git.webp";
-import gsap from "../../public/assets/about/gsap.webp";
-import html from "../../public/assets/about/html.webp";
-import js from "../../public/assets/about/js.webp";
-import NextJs from "../../public/assets/about/NextJs.webp";
-import PocketBase from "../../public/assets/about/PocketBase.webp";
-import php from "../../public/assets/about/php.webp";
-import node from "../../public/assets/about/node.webp";
-import tailwind from "../../public/assets/about/tailwind.webp";
-import python from "../../public/assets/about/python.webp";
-import vite from "../../public/assets/about/vite.webp";
-import sass from "../../public/assets/about/sass.webp";
-import threejs from "../../public/assets/about/three.js.webp";
-import typescript from "../../public/assets/about/typescript.webp";
-import react from "../../public/assets/about/react.webp";
-
-const cards = [
-  {
-    title: "HTML5",
-    description: "Basic HTML (Hyper Text Markup Language)",
-    src: html,
-  },
-  {
-    title: "CSS3",
-    description: "Basic CSS (cascade style sheet)",
-    src: css,
-  },
-  {
-    title: "JavaScript",
-    description: "I have good knowledge of Javascript es6+",
-    src: js,
-  },
-  {
-    title: "React",
-    description: "I use React for single page and highly interactive web apps",
-    src: react,
-  },
-  {
-    title: "TypeScript",
-    description: "I use TypeScript for medium to larger scale web applications",
-    src: typescript,
-  },
-  {
-    title: "Git",
-    description:
-      "I use git to maintain and push my projects to my repositories",
-    src: html,
-  },
-  {
-    title: "SCSS",
-    description: "I use SCSS over CSS because of its extra features",
-    src: sass,
-  },
-  {
-    title: "Tailwind CSS",
-    description:
-      "I use Tailwind if i don't need very fancy css and wanna keep it minimalistic",
-    src: tailwind,
-  },
-  {
-    title: "Node.js",
-    description: "Node.js is my go to JavaScript runtime",
-    src: node,
-  },
-  {
-    title: "PHP",
-    description:
-      "I don't use php in my projects but i have enough knowledge about it",
-    src: php,
-  },
-  {
-    title: "Python",
-    description:
-      "I started programming with python and i am comfortable working with it",
-    src: python,
-  },
-  {
-    title: "Vite",
-    description: "I use vite as my bundler for high performance websites",
-    src: vite,
-  },
-  {
-    title: "Three.js",
-    description:
-      "Three.js is a JavaScript library used to create and display animated 3D computer graphics in the web",
-    src: threejs,
-  },
-  {
-    title: "Gsap",
-    description:
-      "I use Gsap for complex animations and better control over them",
-    src: gsap,
-  },
-  {
-    title: "Next Js",
-    description:
-      "I use Next js for static websites or as my fullstack framework",
-    src: NextJs,
-  },
-  {
-    title: "PocketBase",
-    description:
-      "Open Source backend in 1 file with realtime database, authentication, file storage and admin dashboard",
-    src: PocketBase,
-  },
-];
+import cards from "./aboutData";
 
 export default function About() {
   return (
@@ -118,7 +11,7 @@ export default function About() {
         <p className='text-lg dark:text-gray-400 my-10'>
           I am Bentaleb Sami, a 20 year old algerian computer science student at
           the university of Badji Moukhtar Annaba i am currently third year
-          which means i&aposll get my license diploma this year. <br /> I am
+          which means i&apos;ll get my license diploma this year. <br /> I am
           also mostly a self taught web developer with 3 years of experience
           programming i started out with python, just solving leet code and
           project euler problems and fun projects, then i wanted to specialize
@@ -127,7 +20,7 @@ export default function About() {
           <br />I have made multiple projects which some of them are listed in
           this website some others are in my github and others are retired
           because i have made them in the start of my learning journey so they
-          don&apost show the best version of my code.
+          don&apos;t show the best version of my code.
         </p>
         <h2 className='text-3xl sm:text-4xl lg:text-5xl  dark:text-white '>
           Tools
