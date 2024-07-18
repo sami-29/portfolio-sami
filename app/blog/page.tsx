1fb90a96d299097daf18bdebb7cabdc732799e53
sami-29 <annabisami01@gmail.com>
2024-07-18 21:51:57 +0100

    Refactored blog page component to use Chakra UI's `useColorModeValue` hook and improved layout and styling.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,15 +9,24 @@ export default function Blogs() {
     <PostPreview key={post.slug} {...post} />
   ));
 
-  const textColor = useChakraColorModeValue("gray.800", "white");
+  const textColor = useColorModeValue("gray.800", "white");
 
   return (
-    <Box as="main">
-      <VStack spacing={8} align="start" w={["90%", "75%", "50%"]} mx="auto" mt={24}>
-        <Heading as="h1" fontSize={["4xl", "5xl", "6xl"]} color={textColor} mb={10}>
+    <Box as='main'>
+      <VStack
+        spacing={8}
+        align='start'
+        w={["90%", "75%", "50%"]}
+        mx='auto'
+        mt={24}>
+        <Heading
+          as='h1'
+          fontSize={["4xl", "5xl", "6xl"]}
+          color={textColor}
+          mb={10}>
           Blog posts
         </Heading>
-        <VStack spacing={6} align="stretch" w="full">
+        <VStack spacing={6} align='stretch' w='full'>
           {postPreviews}
         </VStack>
       </VStack>
