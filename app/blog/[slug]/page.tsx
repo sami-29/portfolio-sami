ff4a87d3ccf0f73d99a48cc29a012c787921270e
sami-29 <annabisami01@gmail.com>
2024-07-18 22:31:54 +0100

    Extracted the blog post rendering logic into a reusable `BlogPost` component.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,15 +1,7 @@
-import {
-  Box,
-  VStack,
-  Heading,
-  Text,
-} from "../../../components/ChakraComponents";
-import { useColorModeValue } from "@chakra-ui/react";
 import getPostContent from "../../../utils/GetPostContent";
-import Markdown from "markdown-to-jsx";
 import getPostMetadata from "../../../utils/GetPostMetadata";
 import { urlParamType } from "./UrlType";
-import { Code } from "@chakra-ui/react";
+import BlogPost from "../../../components/BlogPost";
 
 export const generateStaticParams = async () => {
   const posts = getPostMetadata();
@@ -21,51 +13,13 @@ export const generateStaticParams = async () => {
 export default function Post(props: urlParamType) {
   const slug = props.params.slug;
   const post = getPostContent(slug);
-  const textColor = useColorModeValue("gray.800", "gray.200");
-  const subtitleColor = useColorModeValue("gray.600", "gray.400");
 
   return (
-    <Box as='main'>
-      <VStack
-        spacing={8}
-        align='start'
-        w={["90%", "75%", "50%"]}
-        mx='auto'
-        mt={24}>
-        <Heading as='h1' fontSize={["4xl", "5xl", "6xl"]} color={textColor}>
-          {post.data.title}
-        </Heading>
-        <Text fontSize='xl' color={subtitleColor}>
-          {post.data.subtitle}
-        </Text>
-        <Text fontSize='md' color={subtitleColor}>
-          {new Date(post.data.date).toDateString()}
-        </Text>
-        <Box
-          className='markdown-body'
-          color={textColor}
-          fontSize='lg'
-          lineHeight='tall'>
-          <Markdown
-            options={{
-              overrides: {
-                code: ({ children }) => {
-                  return (
-                    <Code
-                      colorScheme='gray'
-                      p={2}
-                      borderRadius='md'
-                      whiteSpace='pre-wrap'>
-                      {children}
-                    </Code>
-                  );
-                },
-              },
-            }}>
-            {post.content}
-          </Markdown>
-        </Box>
-      </VStack>
-    </Box>
+    <BlogPost
+      title={post.data.title}
+      subtitle={post.data.subtitle}
+      date={post.data.date}
+      content={post.content}
+    />
   );
 }
---
+++ components/BlogPost.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Box, VStack, Heading, Text, useColorModeValue } from "@chakra-ui/react";
+import Markdown from "markdown-to-jsx";
+import { Code } from "@chakra-ui/react";
+
+interface BlogPostProps {
+  title: string;
+  subtitle: string;
+  date: string;
+  content: string;
+}
+
+export default function BlogPost({ title, subtitle, date, content }: BlogPostProps) {
+  const textColor = useColorModeValue("gray.800", "gray.200");
+  const subtitleColor = useColorModeValue("gray.600", "gray.400");
+
+  return (
+    <Box as='main'>
+      <VStack
+        spacing={8}
+        align='start'
+        w={["90%", "75%", "50%"]}
+        mx='auto'
+        mt={24}>
+        <Heading as='h1' fontSize={["4xl", "5xl", "6xl"]} color={textColor}>
+          {title}
+        </Heading>
+        <Text fontSize='xl' color={subtitleColor}>
+          {subtitle}
+        </Text>
+        <Text fontSize='md' color={subtitleColor}>
+          {new Date(date).toDateString()}
+        </Text>
+        <Box
+          className='markdown-body'
+          color={textColor}
+          fontSize='lg'
+          lineHeight='tall'>
+          <Markdown
+            options={{
+              overrides: {
+                code: ({ children }) => {
+                  return (
+                    <Code
+                      colorScheme='gray'
+                      p={2}
+                      borderRadius='md'
+                      whiteSpace='pre-wrap'>
+                      {children}
+                    </Code>
+                  );
+                },
+              },
+            }}>
+            {content}
+          </Markdown>
+        </Box>
+      </VStack>
+    </Box>
+  );
+}
