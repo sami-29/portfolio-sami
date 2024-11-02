b298365afaa68d1516beaca0f35312e64fa4a298
sami-29 <annabisami01@gmail.com>
2024-11-02 23:51:44 +0100

    removed vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Providers } from "../components/Providers";
 import Navbar from "../components/MainNavigation/Navbar";
 
 import { Box } from "@chakra-ui/react";
-import { Analytics } from "@vercel/analytics/react";
 
 import Script from "next/script";
 
@@ -12,12 +11,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <Script
         async
         defer
-        src="https://cloud.umami.is/script.js"
-        data-website-id="a8d67507-76c6-42d2-b099-540de21a96af"></Script>
+        src='https://cloud.umami.is/script.js'
+        data-website-id='a8d67507-76c6-42d2-b099-540de21a96af'></Script>
       <body>
         <Providers>
           <Box minHeight='100vh'>
@@ -27,7 +26,6 @@ export default function RootLayout({
             </Box>
           </Box>
         </Providers>
-        <Analytics />
       </body>
     </html>
   );
