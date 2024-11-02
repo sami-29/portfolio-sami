dfd7df87de3614432aeee5e5dbd9bf1d595ec771
sami-29 <annabisami01@gmail.com>
2024-11-02 20:04:12 +0100

    umami analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/MainNavigation/Navbar";
 import ThemeToggle from "../components/ThemeToggle/ThemeToggle";
 import { Box } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Head>
+        <script
+          defer
+          src='https://cloud.umami.is/script.js'
+          data-website-id='a8d67507-76c6-42d2-b099-540de21a96af'></script>
+      </Head>
       <body>
         <Providers>
           <Box minHeight='100vh'>
